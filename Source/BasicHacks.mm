11c10e3791a6f284dbeb16b31b9827b3fb7944c0
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-05 16:37:33 +0300

    Update BasicHacks.mm

--- Source/BasicHacks.mm
+++ Source/BasicHacks.mm
@@ -3,10 +3,32 @@
 
 void* BasicHacks::HacksThread(void* arg)
 {
+    // UnityFramework üzerinden offset ve hex patch tanımı
+    MemoryPatch patch = MemoryPatch::createWithHex("UnityFramework", 0x210EC54, "E0 47 88 52 E0 01 A0 72 C0 03 5F D6");
+
+    bool lastState = false; // Tuşun durumunu takip etmek için
 
     while(KTempVars.running)
     {   
-        usleep(100);
+        // Streamer Mode aktif edildiyse patch uygula, kapatıldıysa eski haline döndür
+        if (KTempVars.StreamerMode) 
+        {
+            if (!lastState && patch.isValid()) 
+            {
+                patch.modify(); // Hex patch'i aktif et
+                lastState = true;
+            }
+        } 
+        else 
+        {
+            if (lastState && patch.isValid()) 
+            {
+                patch.restore(); // Orijinal haline geri döndür
+                lastState = false;
+            }
+        }
+
+        usleep(100000); // İşlemciyi yormamak için 100ms bekleme
     } 
 
     return NULL; 
